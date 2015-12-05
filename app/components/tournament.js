f513ff49d30f5351b5dd3247fad921ef27f4e6a2
jchip <jordanchip@gmail.com>
2015-12-05 13:50:47 -0700

    Initial functionality to Tournament brackets

--- app/components/tournament.js
+++ app/components/tournament.js
@@ -1,5 +1,13 @@
 var React = require("react");
 
+var Button = require('react-bootstrap/lib/Button');
+
+var hideButtonStyle = {
+  visibility: 'hidden'
+};
+var showButtonStyle = {
+  visibility: 'visible'
+}
 
 var minimalData = {
     teams : [
@@ -17,18 +25,65 @@ var Tournament = React.createClass({
   //   $('.tournamentThing').bracket({
   //       init: minimalData /* data to initialize the bracket with */ });
   // },
+
+  getInitialState() {
+    return { showButton: false };
+  },
+
+  saveFn: function(data) {
+    var json = jQuery.toJSON(data);
+    $('#saveOutput').text('POST ' +userData+' '+json);
+  },
   displayBracket: function() {
+    console.log("Displaying Bracket");
+    this.setState({ showButton: true });
+
+    // $('.list-group').bracket({
+    //     init: minimalData /* data to initialize the bracket with */ });
+    // var container = $('list-group')
+    // container.bracket({
+    //   init: minimalData,
+    //   save: saveFn,
+    //   userData: "http://myapi"})
+ 
+    // /* You can also inquiry the current data */
+    // var data = container.bracket('data')
+    // $('#dataOutput').text(jQuery.toJSON(data))
+
     $('.list-group').bracket({
-        init: minimalData /* data to initialize the bracket with */ });
+      init: minimalData,
+      save: function(){}
+    })
+
+    document.getElementById("buttonDiv").style.visibility = 'visible';
+  },
+
+  componentDidMount: function() {
+    document.getElementById("buttonDiv").style.visibility = 'hidden';
+  },
+
+  back: function() {
+    this.setState({ showButton: false  });
   },
   render: function() {
+
     return (
       <div className="container">
-        <h2>List Group With Linked Items</h2>
+        <h2>Tournaments</h2>
         <div className="list-group">
-          <a className="list-group-item active" onClick={this.displayBracket}>First item</a>
+          <a className="list-group-item" onClick={this.displayBracket}>First item</a>
           <a className="list-group-item">Second item</a>
           <a className="list-group-item">Third item</a>
+        </div>
+        <div id="buttonDiv">
+
+          <Button
+            bsStyle="primary"
+            bsSize="large"
+            onClick={this.back}
+          >
+            Back
+          </Button>
         </div>
       </div>
     );
